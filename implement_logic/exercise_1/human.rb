76df41f014ebe23521e4b84f17758958644acde7
Michal Cebula <69808494+michalcebula@users.noreply.github.com>
2021-02-03 14:32:28 +0100

    Update human.rb

--- implement_logic/exercise_1/human.rb
+++ implement_logic/exercise_1/human.rb
@@ -1,2 +1,17 @@
 class Human
-end
+  
+	def introduce_yourself(name, surname)
+		if name != nil and surname != nil
+			puts 'my name is ' + name + surname
+		else
+			puts 'sorry bro my name aint full, can no do'
+		end	
+	end
+
+end 
+
+test = Human.new
+test.introduce_yourself("michal", "cebula")
+
+test.introduce_yourself(nil, nil)
+
